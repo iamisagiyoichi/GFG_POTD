39af4e3d3e853aba1b2b92c80638cc08bb84ac5b
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-20 23:41:59 +0530

    Create Shortest Unique Prefix for Every Word.java

---
+++ Trie/Shortest Unique Prefix for Every Word.java
@@ -0,0 +1,41 @@
+class Solution {
+    class TrieNode {
+        TrieNode[] child = new TrieNode[26];
+        int freq;
+    }
+
+    public ArrayList<String> findPrefixes(String[] arr) {
+        TrieNode root = new TrieNode();
+
+        for (String word : arr) {
+            TrieNode curr = root;
+            for (char ch : word.toCharArray()) {
+                int idx = ch - 'a';
+                if (curr.child[idx] == null) {
+                    curr.child[idx] = new TrieNode();
+                }
+                curr = curr.child[idx];
+                curr.freq++;
+            }
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (String word : arr) {
+            TrieNode curr = root;
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                curr = curr.child[ch - 'a'];
+                sb.append(ch);
+                if (curr.freq == 1) {
+                    break;
+                }
+            }
+
+            ans.add(sb.toString());
+        }
+
+        return ans;
+    }
+}
