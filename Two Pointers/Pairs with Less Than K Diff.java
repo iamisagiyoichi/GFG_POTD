f09ef9f1ef22870dc6cbbd166a83d1649354de95
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-08-04 23:40:57 +0530

    Create Pairs with Less Than K Diff.java

---
+++ Two Pointers/Pairs with Less Than K Diff.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int countPairs(int arr[], int k) {
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        int j = 0;
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (j < n && arr[j] - arr[i] < k) {
+                j++;
+            }
+            ans += j - i - 1;
+        }
+
+        return (int) ans;
+    }
+}
